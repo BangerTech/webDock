8f247c0d8bfe39ec23481a3f14f978a1b208da06
Bangertech <info@bangertech.de>
2025-03-15 12:41:13 +0100

    updated drag&drop v.8

--- docker-templates/webdock-ui/src/app.py
+++ docker-templates/webdock-ui/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, send_from_directory, abort, request
+from flask import Flask, jsonify, render_template, send_from_directory, abort, request, Response
 import os
 import logging
 import yaml
