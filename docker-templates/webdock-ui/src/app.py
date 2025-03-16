0585653138234f12a263f3deb434e114f90151ab
Bangertech <info@bangertech.de>
2025-03-16 18:02:38 +0100

    drag&drop fix v.5

--- docker-templates/webdock-ui/src/app.py
+++ docker-templates/webdock-ui/src/app.py
@@ -216,12 +216,12 @@ else:
 # Konfiguriere die Pfade relativ zum Basis-Pfad
 CONFIG_DIR = os.getenv('CONFIG_DIR', os.path.join(WEBDOCK_BASE_PATH, 'config'))
 TEMPLATE_DIR = os.path.join(APP_DIR, 'config')
-
-# Verwende die bestehende Kategoriedatei im webdock-ui Verzeichnis
-CATEGORIES_FILE = os.path.join(COMPOSE_DATA_DIR, 'webdock-ui', 'src', 'config', 'categories.yaml')
 COMPOSE_DIR = os.path.join(CONFIG_DIR, 'compose-files')
 COMPOSE_FILES_DIR = os.getenv('COMPOSE_FILES_DIR', os.path.join(WEBDOCK_BASE_PATH, 'webdock-templates'))
 COMPOSE_DATA_DIR = os.getenv('COMPOSE_DATA_DIR', os.path.join(WEBDOCK_BASE_PATH, 'webdock-data'))
+
+# Verwende die bestehende Kategoriedatei im webdock-ui Verzeichnis
+CATEGORIES_FILE = os.path.join(COMPOSE_DATA_DIR, 'webdock-ui', 'src', 'config', 'categories.yaml')
 
 # Logge die wichtigen Pfade
 logger.info(f"WebDock Base Path: {WEBDOCK_BASE_PATH}")
