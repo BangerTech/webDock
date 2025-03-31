99f7bebd3be3317d58e158aef5156ea30729cfb2
Bangertech <info@bangertech.de>
2025-03-31 12:19:34 +0200

    updated watchyourlan scanning function v.1

--- docker-templates/webdock-ui/src/app.py
+++ docker-templates/webdock-ui/src/app.py
@@ -4152,6 +4152,7 @@ services:
       IGNOREIP: "no"
       NETWORK_INTERFACE: "{env_vars['NETWORK_INTERFACE']}"
       IP_RANGE: "{env_vars['IP_RANGE']}"
+      ARP_STRS: "-I {env_vars['NETWORK_INTERFACE']} {env_vars['IP_RANGE']}"
 
 networks:
   webdock-network:
@@ -9690,6 +9691,7 @@ services:
       IGNOREIP: "no"
       NETWORK_INTERFACE: "{env_vars['NETWORK_INTERFACE']}"
       IP_RANGE: "{env_vars['IP_RANGE']}"
+      ARP_STRS: "-I {env_vars['NETWORK_INTERFACE']} {env_vars['IP_RANGE']}"
 
 networks:
   webdock-network:
